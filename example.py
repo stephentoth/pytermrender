03be2c5f3ad49d9da6d6f9e4dbfeffb2e182f0cf
icaema <stephentoth.st@gmail.com>
2023-11-16 20:00:06 -0500

    cleaned up some stuff and added Documentation in README.md

--- example.py
+++ example.py
@@ -1,28 +1,38 @@
 from pytermrender import *
+from colorama import Fore, Back, Style
+
 Screen = TermScreen(height=24, width=80, framerate=30)
 
-#not strictly needed, but useful
-def tick():
-    Screen.frame_no+=1
 
 def setup():
     print("Setup")
     print("Clearing")
     clearScreen()
-    pass
+    return
 
 
 def teardown():
+    clearScreen()
     print("Teardown")
     print("Teardown Complete")
-    pass
+    return
 
+
+#not strictly needed, but useful
+def tick():
+    Screen.frame_no+=1
+    return
 
 def draw():
     clearScreen()
     clearAllBuffers()
+    frame = Screen.frame_no
+
     drawBox(0,0,Screen.width,Screen.height,'#')
-    printBuffer(1,1,str(Screen.framerate)+'fps '+str(Screen.frame_no))
+    printBuffer(1,1,str(Screen.framerate)+'fps -'+str(frame)+'-')
+    for ix, c in enumerate(str(frame)):
+        putBuffer(8+ix,1, Fore.BLUE, buffer="color")
+    return
 
 
 run()
--- pytermrender.py
+++ pytermrender.py
@@ -1,16 +1,11 @@
-import __main__
-import time, math
-from colorama import Fore, Back, Style
-import functools
-#from dataclasses import dataclass
-printr = functools.partial(print, end="")
+import __main__, time, math, functools
+from colorama import Style
 
 ## Definitions
-
+printr = functools.partial(print, end="")
 
 class RenderException(Exception):
     pass
-
 
 class TermScreen:
     width: int
@@ -19,14 +14,17 @@ class TermScreen:
     frame_no: int = 0
     buffers: list = ["char", "color"]
     tickers: dict
+    state: dict
     def __init__(self, *, width: int = 80, height: int = 24, framerate: float = 30.0):
         self.width=width
         self.height=height
         self.framerate=framerate
         self.buffer_char  = [[" "]*self.width for i in range(self.height)]
         self.buffer_color = [[Style.RESET_ALL]*self.width for i in range(self.height)]
+        self.tickers = {}
+        self.state = {}
         return
-
+    
     def reset_buffer(self, buffer="char"):
         if buffer not in self.buffers:
             raise RenderException("Buffer Does not exist")
@@ -37,6 +35,31 @@ class TermScreen:
                 self.buffer_color = [[Style.RESET_ALL]*self.width for i in range(self.height)]
         return
     
+    def reset_buffer_line(self, line=0, buffer="char"):
+        if buffer not in self.buffers:
+            raise RenderException("Buffer Does not exist")
+        if line >= self.height:
+            raise RenderException("Line is out of range of height")
+        match buffer:
+            case "char":
+                self.buffer_char[line]  = [" "]*self.width
+            case "color":
+                self.buffer_color[line] = [Style.RESET_ALL]*self.width
+        return
+
+    def put_buffer(self, x, y, val, buffer="char"):
+        if buffer not in self.buffers:
+            raise RenderException("Buffer Does not exist")
+        if x >= self.width or y >= self.height:
+            raise RenderException("Point is out of range of the buffer")
+        char=str(val)
+        match buffer:
+            case "char":
+                self.buffer_char[y][x]  = char
+            case "color":
+                self.buffer_color[y][x]  = char
+        return
+
     def render(self):
         for y in range(self.height):
             for x in range(self.width):
@@ -44,6 +67,9 @@ class TermScreen:
                 printr(self.buffer_char[y][x])
             print() #print \n
         return
+
+
+
 
 def framerateMaker(func, perSec):
     interval = 1.0 / perSec
@@ -66,11 +92,11 @@ def renderLoop():
 ## Library Functions
 
 def clearScreen():
-    printr('\033[2J')
+    printr('\033[2J\r')
 
-def clearBuffer(buf="char"):
+def clearBuffer(buffer=TermScreen.buffers[0]):
     global Screen
-    Screen.reset_buffer(buf)
+    Screen.reset_buffer(buffer)
     return
 
 def clearAllBuffers():
@@ -79,19 +105,28 @@ def clearAllBuffers():
         Screen.reset_buffer(buf)
     return
 
+def clearLine(y, buffer=TermScreen.buffers[0]):
+    global Screen
+    Screen.reset_buffer_line(y, buffer)
+    return
+
+def putBuffer(x, y, val, *, buffer=TermScreen.buffers[0]):
+    global Screen
+    Screen.put_buffer(x, y, val, buffer)
+    return
+
 def printBuffer(x, y, val):
+    """ Print a string into the char buffer """
     global Screen
     string=str(val)
     if len(string) > Screen.width-x:
         raise RenderException("Too Long to fit in screen")
     for i, char in enumerate(string):
         Screen.buffer_char[y][x+i]=char
-
-def clearLine(y):
-    global Screen
-    Screen.buffer_char[y]=[" "]*Screen.width
+    return
 
 def drawBox(x,y,width,height,char):
+    """ Draw a box in the char buffer """
     global Screen
     if x+width > Screen.width or y+height > Screen.height:
         raise RenderException("Box too BIG")
@@ -99,17 +134,16 @@ def drawBox(x,y,width,height,char):
         for w_y in range(height):
             if (w_x==0 or w_x==width-1) or (w_y==0 or w_y==height-1):
                 Screen.buffer_char[y+w_y][x+w_x] = char
+    return
 
 ####################
-
-
 
 def tick():
     global Screen
     Screen.frame_no+=1
 
 def setup():
-    print("Setup")
+    print("Default Setup")
     clearScreen()
     clearAllBuffers()
     return
